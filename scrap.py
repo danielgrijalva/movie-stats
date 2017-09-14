7385067b3da37ed95b242140f97355ce23a19150
Juanets <jdgs.gt@gmail.com>
2017-09-13 21:32:32 -0700

    Using pandas.read_html() instead of BeautifulSoup4

--- scrap.py
+++ scrap.py
@@ -1,6 +1,4 @@
-from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
 import requests
 import time
 
@@ -16,39 +14,43 @@ def get_html(year):
         html: HTML string.
     '''
     html = requests.get(url.format(year=year)).content
-    time.sleep(1)
 
     return html
 
-def get_gross(html):
-    '''Parses HTML data using bs4 to get exact info about movie revenue.
+
+def read_html(html):
+    '''Using pandas.read_html() to extract table inside HTML string.
 
     Args:
-        html: text containting HTML webpage.
+        html: HTML string returned from get_html()
 
     Returns:
-        gross: list of ints (movie revenue).
+        box_office: DataFrame object containing revenue info of 50 movies.
     '''
-    soup = BeautifulSoup(html, 'html.parser')
-    movies = soup.find_all('p', {'class': 'sort-num_votes-visible'})[:10]
+    data = pd.read_html(html)[1]
+    box_office = data[['Domestic Box Office']][:50]
 
-    gross = [int(movie.find_all('span')[4]['data-value'].replace(',', '')) for movie in movies]
+    return box_office
 
-    return gross
 
-def group_data(year, gross):
+def format_currency(dataframe):
+    '''Remove '$' and ',' from each row.'''
+    dataframe = dataframe.replace('[\$,]', '', regex=True).astype(int)
+    return dataframe
+
+def group_data(year, avg_gross):
     '''Groups year and average gross into a dict.'''
-    data = {'year': [year,], 'gross': [gross,]}
+    data = {'year': [year,], 'gross': [avg_gross,]}
 
     return data
 
 
 def main():
-    '''Generate dataframe with all the data and save it as csv.'''
+
     data = pd.DataFrame()
 
     for i in range(1980, 2017):
-        gross = np.mean(get_gross(get_html(i)))
+        gross = int(format_currency((read_html(get_html(i)))).mean())
         year_gross_dict = group_data(i, gross)
 
         df = pd.DataFrame.from_dict(year_gross_dict)
@@ -56,7 +58,6 @@ def main():
 
     data.reset_index(drop=True, inplace=True)
     data.to_csv('data.csv')
-
 
 
 if __name__=='__main__':
